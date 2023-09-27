427011a6bc752ca02e4d31190308cd7a9f70aae4
Soynexoo <Soynexoo@example.com>
2023-09-27 14:46:49 -0300

    Navbar falta search

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-// import './App.css'
+import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomeView from './Views/HomeView/HomeView'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 
 function App() {
-
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<><HomeView/><Footer/></>}/>
+        <Route
+          path="/"
+          element={
+            <>
+              <HomeView />
+              <Footer />
+            </>
+          }
+        />
       </Routes>
     </>
   )
--- src/Views/HomeView/HomeView.jsx
+++ src/Views/HomeView/HomeView.jsx
@@ -1,8 +1,6 @@
 // import React from 'react'
 
 const HomeView = () => {
-  return (
-    <div>HomeView</div>
-  )
+  return <div style={{ background: '#a1a1a1', height: '100vh' }}>HomeView</div>
 }
 export default HomeView
