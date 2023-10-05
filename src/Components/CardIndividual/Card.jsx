400e0ce3a44b45334df780ac66b008ca17e9a1bc
Soynexoo <Soynexoo@example.com>
2023-10-05 11:29:04 -0300

    AGOTADA

--- src/Components/CardIndividual/Card.jsx
+++ src/Components/CardIndividual/Card.jsx
@@ -5,38 +5,22 @@ const Card = ({ title, price, author, image, id, availability }) => {
   return (
     <div
       style={{
-        background: "white",
+        background: 'white',
         padding: '20px',
         paddingBottom: '0',
         maxHeight: '530px',
         width: '200px',
         boxShadow:
           '0 10px 15px -3px rgba(0,0,0,.1),0 4px 6px -4px rgba(0,0,0,.1 )',
-        position: 'relative', 
-        }}
+        position: 'relative',
+      }}
       className={style.card}
     >
       {!availability && (
-    <div
-      style={{
-        position: 'absolute',
-        left: '-15px',
-        top: '30px',// Ajustar la posición horizontal del moño
-        background: 'red',
-        width:"120px",
-        color: 'white',
-        padding: '5px',
-        borderRadius: '5px',
-        textAlign:"center",
-        fontSize: '15px',
-        transform: 'rotate(-45deg)', // Rotar el elemento en -45 grados 
-        overflow: 'hidden',
-        textTransform: 'uppercase',
-      }}
-    >
-      Agotado
-    </div>
-  )}
+        <div className={`${style.ribbon} ${style.ribbonTopRight}`}>
+          <span>agotada</span>
+        </div>
+      )}
       <div
         style={{ display: 'flex', margin: 'auto 0', justifyContent: 'center' }}
       >
