8e760d989c2bb0237f8e2df33df96263cd0f74f8
debian00 <debianstark@gmail.com>
2023-10-13 19:39:22 -0600

    replaced aboutus with contact form

--- src/Views/AboutUsView/AboutUsView.jsx
+++ src/Views/AboutUsView/AboutUsView.jsx
@@ -10,15 +10,22 @@ const AboutUs = () => {
     <div className={`row ${style.content}`} >
       <div className={`col-12 ${style.card}`} >
         <p className={style.texto}>The Next Page es una librería en línea apasionada por la literatura, comprometida con conectar amantes de la lectura con historias inspiradoras y conocimientos profundos.</p>
-        <p className={style.texto}>Nacimos de la pasión compartida por la lectura, creando una plataforma global para explorar y adquirir una amplia gama de libros.</p>
-        <p className={style.texto}>Creemos que los libros enriquecen nuestras vidas y amplían horizontes, ofreciendo una selección cuidadosamente curada en diversos géneros y una comunidad de lectores apasionados.</p>
-        <p className={style.texto}>Nuestra plataforma es segura y fácil de navegar, colaboramos con editores y autores destacados, y te invitamos a unirte a nuestra comunidad de lectores. ¡Descubre tu próxima aventura literaria en The Next Page!</p>
+        
       </div>
       <div className={`col-12 ${style.card}`} >
-        <p className={style.texto}>The Next Page es una librería en línea apasionada por la literatura, comprometida con conectar amantes de la lectura con historias inspiradoras y conocimientos profundos.</p>
-        <p className={style.texto}>Nacimos de la pasión compartida por la lectura, creando una plataforma global para explorar y adquirir una amplia gama de libros.</p>
-        <p className={style.texto}>Creemos que los libros enriquecen nuestras vidas y amplían horizontes, ofreciendo una selección cuidadosamente curada en diversos géneros y una comunidad de lectores apasionados.</p>
-        <p className={style.texto}>Nuestra plataforma es segura y fácil de navegar, colaboramos con editores y autores destacados, y te invitamos a unirte a nuestra comunidad de lectores. ¡Descubre tu próxima aventura literaria en The Next Page!</p>
+      <form action="#">
+    <label for="customerName">NOMBRE <em>&#x2a;</em></label>
+    <input id="customerName" name="customerName" required="" type="text" />
+    <label for="customerEmail">EMAIL <em>&#x2a;</em>
+    </label>
+    <input id="customerEmail" name="customerEmail" required="" type="email" /><label for="customerPhone">TELÉFONO</label><input id="customerPhone" name="customerPhone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" type="tel" /><label for="orderNumber">NÚMERO DE ORDEN</label><input id="orderNumber" name="orderNumber" type="text" /><label for="customerNote">TU MENSAJE: <em>&#x2a;</em></label><textarea id="customerNote" name="customerNote" required="" rows="4"></textarea>
+    <h3>
+      Por favor proporciona toda la informacion necesaria.
+      </h3> 
+      
+<button id="customerOrder">Enviar</button>
+  </form>
+ 
       </div>
     </div>
   </div>
